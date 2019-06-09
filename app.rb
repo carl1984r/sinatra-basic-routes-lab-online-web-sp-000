d3fafbb9169c76f13ca68b3112f0d4ee45a9750e
Bry Cox <carl.cc@gmail.com>
2019-06-09 13:50:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ class App < Sinatra::Base
   get '/name' do
     # some code to get all the medicines
     # some code to render the correct HTML page
- end
+  end
+
+  get '/hometown' do
+    # some code to get all the medicines
+    # some code to render the correct HTML page
+  end
 
 end
