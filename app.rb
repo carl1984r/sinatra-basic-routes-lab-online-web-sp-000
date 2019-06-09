36bd60d44990481dbf31d80bfe17ef62263ec9ac
Bry Cox <carl.cc@gmail.com>
2019-06-09 13:53:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,19 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    # some code to get all the medicines
-    # some code to render the correct HTML page
     "My name is "
   end
 
   get '/hometown' do
-    # some code to get all the medicines
-    # some code to render the correct HTML page
+    "My hometown is "
   end
 
   get '/favorite-song' do
-    # some code to get all the medicines
-    # some code to render the correct HTML page
+    "My favorite song is "
   end
 
 end
