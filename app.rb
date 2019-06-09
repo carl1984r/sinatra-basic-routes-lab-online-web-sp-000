28f992d48e89b442750b1ec7346ca5698b441ba7
Bry Cox <carl.cc@gmail.com>
2019-06-09 13:50:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    # some code to get all the medicines
+    # some code to render the correct HTML page
+ end
+ 
 end
